45cc8e131b09a8e7bb01e21e9619841761bec940
Ray Wolt <rjwolt@gmail.com>
2023-08-02 21:39:39 -0500

    Add checkbox to options to show or hide sidebar and event listener to save UI options to Chrome sync

--- content-script.js
+++ content-script.js
@@ -1,71 +1,60 @@
 function deleteCommentAndLogo() {
-    try{
-        document.getElementsByClassName('_30BbATRhFv3V83DHNDjJAO')[0].innerHTML = ""
-        
-    }
-    catch(err) {
-        
-    }
+  try {
+    document.getElementsByClassName("_30BbATRhFv3V83DHNDjJAO")[0].innerHTML =
+      "";
+  } catch (err) {}
 
-    try{
-        document.getElementsByClassName('_2l7c_Oz0UVsamALvPrlznq')[0].innerHTML = ""
-    }
-    catch(err) {
-
-    }
+  try {
+    document.getElementsByClassName("_2l7c_Oz0UVsamALvPrlznq")[0].innerHTML =
+      "";
+  } catch (err) {}
 }
 
-chrome.storage.sync.get(['userData'], function(result) {
-    whiteList = [];
-    blackList = [];
-    if (result.userData != undefined)
-    {
-        whiteList = result.userData.whiteList;
-        blackList = result.userData.blackList;
-    }
+chrome.storage.sync.get(["userData"], function (result) {
+  whiteList = [];
+  blackList = [];
+  showSideBar = true;
+  if (result.userData != undefined) {
+    whiteList = result.userData.whiteList;
+    blackList = result.userData.blackList;
+    showSideBar = result.userDaa.showSideBar;
+  }
 
+  pathName = window.location.pathname;
 
-    pathName = window.location.pathname;
-
-    // check if not a comment section, and not a whitelisted subreddit
-    if (pathName != null )
-    {
-        if (pathName.includes("comments"))
-        {
-            // check the blacklist to see
-            blackList.forEach(element => {
-                if (pathName.toLowerCase().includes("/r/" + element.toLowerCase()))
-                {
-                    //document.getElementsByTagName('body')[0].innerHTML = "<k>STOP PROCRASTINATING ON REDDIT</k> <p>You may only go to reddit comment pages</p> <p>Extension built by <a style=\"color:blue\" href=\"https://henryz.dev/\">Henry Zhang</a></p>";
-                    window.location.href = "https://hz757.github.io/PortfolioWebsite/RedditLiberationRedirect.html";
-                }
-            })
+  // check if not a comment section, and not a whitelisted subreddit
+  if (pathName != null) {
+    if (pathName.includes("comments")) {
+      // check the blacklist to see
+      blackList.forEach((element) => {
+        if (pathName.toLowerCase().includes("/r/" + element.toLowerCase())) {
+          //document.getElementsByTagName('body')[0].innerHTML = "<k>STOP PROCRASTINATING ON REDDIT</k> <p>You may only go to reddit comment pages</p> <p>Extension built by <a style=\"color:blue\" href=\"https://henryz.dev/\">Henry Zhang</a></p>";
+          window.location.href =
+            "https://hz757.github.io/PortfolioWebsite/RedditLiberationRedirect.html";
         }
-        else
-        {
-            console.log("2");
-            // check each member of the whitelist, if the website isn't there, block it
-            willBlock = true;
-            whiteList.forEach(element => {
-                if (pathName.toLowerCase().includes("/r/" + element.toLowerCase()))
-                {
-                    willBlock = false;
-                }
-            })
-            
-            // I need to do testing to see if this works, because it could very well not work
-            if (willBlock)
-            {
-                //document.getElementsByTagName('body')[0].innerHTML = "<k>STOP PROCRASTINATING ON REDDIT</k> <p>You may only go to reddit comment pages</p> <p>Extension built by <a style=\"color:blue\" href=\"https://henryz.dev/\">Henry Zhang</a></p>";
-                window.location.href = "https://hz757.github.io/PortfolioWebsite/RedditLiberationRedirect.html";
-            }
+      });
+    } else {
+      console.log("2");
+      // check each member of the whitelist, if the website isn't there, block it
+      willBlock = true;
+      whiteList.forEach((element) => {
+        if (pathName.toLowerCase().includes("/r/" + element.toLowerCase())) {
+          willBlock = false;
         }
-    }
-    else
-    {
+      });
+
+      // I need to do testing to see if this works, because it could very well not work
+      if (willBlock) {
         //document.getElementsByTagName('body')[0].innerHTML = "<k>STOP PROCRASTINATING ON REDDIT</k> <p>You may only go to reddit comment pages</p> <p>Extension built by <a style=\"color:blue\" href=\"https://henryz.dev/\">Henry Zhang</a></p>";
-        window.location.href = "https://hz757.github.io/PortfolioWebsite/RedditLiberationRedirect.html";
+        window.location.href =
+          "https://hz757.github.io/PortfolioWebsite/RedditLiberationRedirect.html";
+      }
     }
+  } else {
+    //document.getElementsByTagName('body')[0].innerHTML = "<k>STOP PROCRASTINATING ON REDDIT</k> <p>You may only go to reddit comment pages</p> <p>Extension built by <a style=\"color:blue\" href=\"https://henryz.dev/\">Henry Zhang</a></p>";
+    window.location.href =
+      "https://hz757.github.io/PortfolioWebsite/RedditLiberationRedirect.html";
+  }
 
-    setInterval(deleteCommentAndLogo, 500)
+  setInterval(deleteCommentAndLogo, 500);
 });
--- options.js
+++ options.js
@@ -1,108 +1,120 @@
+chrome.storage.sync.get(["userData"], function (result) {
+  whiteList = [];
+  blackList = [];
+  userInterfaceOptions = {};
+  if (result.userData != undefined) {
+    whiteList = result.userData.whiteList;
+    blackList = result.userData.blackList;
+    userInterfaceOptions = result.userData.userInterfaceOptions || {};
+  }
 
-chrome.storage.sync.get(['userData'], function(result) {
-    whiteList = [];
-    blackList = [];
-    if (result.userData != undefined)
-    {
-        whiteList = result.userData.whiteList;
-        blackList = result.userData.blackList;
+  whiteListElement = document.getElementById("whiteList");
+  blackListElement = document.getElementById("blackList");
+
+  // populate the lists
+  whiteList.forEach((element) => {
+    whiteListElement.innerHTML +=
+      '<button class="whiteElement">' + element + " X</button>";
+  });
+
+  blackList.forEach((element) => {
+    blackListElement.innerHTML +=
+      '<button class="blackElement">' + element + " X</button>";
+  });
+
+  var addList = function () {
+    if (this.id == "addWhiteListButton") {
+      whiteList.push(
+        document.getElementById("addWhiteList").value.toLowerCase()
+      );
+      whiteListElement.innerHTML +=
+        '<button class="whiteElement">' +
+        document.getElementById("addWhiteList").value.toLowerCase() +
+        " X</button>";
+      document.getElementById("addWhiteList").value = "";
+
+      userData = { whiteList: whiteList, blackList: blackList };
+      chrome.storage.sync.set({ userData: userData }, function () {
+        //console.log('Value is set to ' + userData)
+      });
+      document.getElementById("addWhiteListButton").onclick = addList;
+    } else {
+      blackList.push(
+        document.getElementById("addBlackList").value.toLowerCase()
+      );
+      blackListElement.innerHTML +=
+        '<button class="blackElement">' +
+        document.getElementById("addBlackList").value.toLowerCase() +
+        " X</button>";
+      document.getElementById("addBlackList").value = "";
+
+      userData = { whiteList: whiteList, blackList: blackList };
+      chrome.storage.sync.set({ userData: userData }, function () {
+        //console.log('Value is set to ' + userData)
+      });
+      document.getElementById("addBlackListButton").onclick = addList;
     }
-
-    whiteListElement = document.getElementById("whiteList");
-    blackListElement = document.getElementById("blackList");
-
-    // populate the lists
-    whiteList.forEach(element => {
-        whiteListElement.innerHTML += "<button class=\"whiteElement\">" + element +  " X</button>"
-    })
-
-    blackList.forEach(element => {
-        blackListElement.innerHTML += "<button class=\"blackElement\">" + element + " X</button>"
-    })
-
-    var addList = function()
-    {
-        if (this.id == "addWhiteListButton")
-        {
-            whiteList.push(document.getElementById("addWhiteList").value.toLowerCase())
-            whiteListElement.innerHTML += "<button class=\"whiteElement\">" + document.getElementById("addWhiteList").value.toLowerCase() +  " X</button>"
-            document.getElementById("addWhiteList").value = ""
-
-            userData = {whiteList: whiteList, blackList: blackList}
-            chrome.storage.sync.set({userData: userData}, function() {
-                //console.log('Value is set to ' + userData)
-            });
-            document.getElementById("addWhiteListButton").onclick = addList;
-        }
-        else
-        {
-            blackList.push(document.getElementById("addBlackList").value.toLowerCase())
-            blackListElement.innerHTML += "<button class=\"blackElement\">" + document.getElementById("addBlackList").value.toLowerCase() +  " X</button>"
-            document.getElementById("addBlackList").value = ""
-
-            userData = {whiteList: whiteList, blackList: blackList}
-            chrome.storage.sync.set({userData: userData}, function() {
-                //console.log('Value is set to ' + userData)
-            });
-            document.getElementById("addBlackListButton").onclick = addList;
-        }
-
-        let whiteElements = document.getElementsByClassName("whiteElement");
-
-        for(let i = 0; i < whiteElements.length; i++) {
-            whiteElements[i].onclick = removeFromList
-        }
-
-        let blackElements = document.getElementsByClassName("blackElement");
-
-        for(let i = 0; i < blackElements.length; i++) {
-            blackElements[i].onclick = removeFromList
-        }
-    }
-
-    var removeFromList = function()
-    {
-        subredditName = this.innerText.substring(0, this.innerText.length-2);
-        var isSubredditname = (element) => element == subredditName;
-        // find the subreddit name whiteList/blackList and remove it from there
-        // update google storage
-        // remove the element from the options page
-
-        if (this.className == "whiteElement")
-        {
-            whiteList.splice(whiteList.findIndex(isSubredditname), 1);
-            userData = {whiteList: whiteList, blackList: blackList}
-            chrome.storage.sync.set({userData: userData}, function() {
-                
-            });
-            this.remove();
-            
-        }
-        else
-        {
-            blackList.splice(blackList.findIndex(isSubredditname), 1);
-            userData = {whiteList: whiteList, blackList: blackList}
-            chrome.storage.sync.set({userData: userData}, function() {
-                
-            });
-            this.remove();
-            
-        }
-    }
-
-    document.getElementById("addWhiteListButton").onclick = addList;
-    document.getElementById("addBlackListButton").onclick = addList;
-
 
     let whiteElements = document.getElementsByClassName("whiteElement");
 
-    for(let i = 0; i < whiteElements.length; i++) {
-        whiteElements[i].onclick = removeFromList
+    for (let i = 0; i < whiteElements.length; i++) {
+      whiteElements[i].onclick = removeFromList;
     }
 
     let blackElements = document.getElementsByClassName("blackElement");
 
-    for(let i = 0; i < blackElements.length; i++) {
-        blackElements[i].onclick = removeFromList
+    for (let i = 0; i < blackElements.length; i++) {
+      blackElements[i].onclick = removeFromList;
     }
+  };
+
+  var removeFromList = function () {
+    subredditName = this.innerText.substring(0, this.innerText.length - 2);
+    var isSubredditname = (element) => element == subredditName;
+    // find the subreddit name whiteList/blackList and remove it from there
+    // update google storage
+    // remove the element from the options page
+
+    if (this.className == "whiteElement") {
+      whiteList.splice(whiteList.findIndex(isSubredditname), 1);
+      userData = { whiteList: whiteList, blackList: blackList };
+      chrome.storage.sync.set({ userData: userData }, function () {});
+      this.remove();
+    } else {
+      blackList.splice(blackList.findIndex(isSubredditname), 1);
+      userData = { whiteList: whiteList, blackList: blackList };
+      chrome.storage.sync.set({ userData: userData }, function () {});
+      this.remove();
+    }
+  };
+
+  const toggleUserInterfaceOption = (userInterfaceOption, value) => {
+    userInterfaceOptions = {
+      ...userInterfaceOptions,
+      [userInterfaceOption]: value,
+    };
+    userData = { whiteList, blackList, userInterfaceOptions };
+    chrome.storage.sync.set({ userData });
+  };
+
+  document.querySelectorAll(".ui-option").forEach((element) =>
+    element.addEventListener("click", (e) => {
+      toggleUserInterfaceOption(e.target.id, e.target.checked);
+    })
+  );
+
+  document.getElementById("addWhiteListButton").onclick = addList;
+  document.getElementById("addBlackListButton").onclick = addList;
+
+  let whiteElements = document.getElementsByClassName("whiteElement");
+
+  for (let i = 0; i < whiteElements.length; i++) {
+    whiteElements[i].onclick = removeFromList;
+  }
+
+  let blackElements = document.getElementsByClassName("blackElement");
+
+  for (let i = 0; i < blackElements.length; i++) {
+    blackElements[i].onclick = removeFromList;
+  }
 });
